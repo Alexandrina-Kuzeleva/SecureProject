15fdbe533a19925edbe7b8dee529a3a7e38efc2b
Alexandrina-Kuzeleva <alaz2005tv@gmail.com>
2026-04-01 17:36:13 +0300

    Task 6:Init XSS

---
+++ reports/report_06.md
@@ -0,0 +1,7 @@
+# Report Task6
+
+## 1. Alert доказательство уязвимости
+
+![1](screenshots/task_6/alert.gif)
+
+![1.1](screenshots/task_6/alert.png)
---
+++ src/comments_app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+comments = []
+
+@app.get("/comments")
+def get_comments(request: Request):
+    return templates.TemplateResponse("comments.html", {
+        "request": request,
+        "comments": comments
+    })
+
+@app.post("/comments")
+def post_comment(comment: str = Form(...)):
+    comments.append(comment)
+    return RedirectResponse(url="/comments", status_code=303)
